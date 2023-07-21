d5b746a107954a2de8768e71db519a90cc5b0eb4
ashishh-tomar <ashishtomar692@gmail.com>
2023-07-21 12:29:40 +0530

    Added Review operations like created review model and get all reviews, get a single review and update seeder, add review for bootcamp, calculate average rating and update & delete review.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,7 +13,8 @@ const Bootcamp = require('../models/Bootcamp')
 
 
 //Include other resource routers
-const courseRouter = require('./courses')
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -23,6 +24,7 @@ const {protect , authorize} = require('../middleware/auth')
 
 //Re-route into other resource routers
 router.use('/:bootcampId/courses',courseRouter);
+router.use('/:bootcampId/reviews',reviewRouter);
 
 
 router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(protect,authorize('publisher','admin'),createBootcamp);
